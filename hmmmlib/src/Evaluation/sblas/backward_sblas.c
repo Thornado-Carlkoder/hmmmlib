950c53b2a28e727dcdeaa6039740ced4ba04c3b7
thornado123 <thor@chora.dk>
2019-11-16 20:32:18 +0100

    RSB Backward

--- hmmmlib/src/Evaluation/sblas/backward_sblas.c
+++ hmmmlib/src/Evaluation/sblas/backward_sblas.c
@@ -4,6 +4,54 @@
 //#include <Accelerate/Accelerate.h> // for mac os
 #include <cblas.h> // for GNUlinux
 
+void b_emission_rsb_mtx(HMM * hmm, double ** sparseMatrixs, struct rsb_mtx_t ** rsb_mtx, rsb_err_t * errval){
+    
+    rsb_type_t typecode = RSB_NUMERICAL_TYPE_DEFAULT;
+    const int brA = RSB_DEFAULT_BLOCKING;
+    const int bcA = RSB_DEFAULT_BLOCKING;
+    
+    unsigned int i;
+    unsigned int j;
+    
+    int * ia = calloc(hmm->hiddenStates*hmm->hiddenStates, sizeof(int));
+    int * ja = calloc(hmm->hiddenStates*hmm->hiddenStates, sizeof(int));
+    
+    int nnz = 0;
+    
+    for (j = 0; j < hmm->hiddenStates; j++) {
+        for (i = 0; i < hmm->hiddenStates; i++) {
+            if (sparseMatrixs[0][j*hmm->hiddenStates+i] != 0.0) {
+                ja[nnz] = i;
+                ia[nnz] = j;
+                nnz++;
+            }
+        }
+    }
+    
+    int * final_ja = malloc(nnz*sizeof(int));
+    int * final_ia = malloc(nnz*sizeof(int));
+    double * final_a = malloc(nnz*sizeof(double));
+    
+    for(i = 0; i < nnz; i++){
+        final_ja[i] = ja[i];
+        final_ia[i] = ia[i];
+    }
+    
+    free(ia);
+    free(ja);
+
+    for(i = 0; i < hmm->observations; i++){
+        for(j = 0; j < nnz; j++){
+            final_a[j] = sparseMatrixs[i][final_ia[j]*hmm->hiddenStates+final_ja[j]];
+        }
+        rsb_mtx[i] = rsb_mtx_alloc_from_coo_const(final_a, final_ia, final_ja, nnz, typecode, hmm->hiddenStates, hmm->hiddenStates, brA, bcA, RSB_FLAG_NOFLAGS | RSB_FLAG_DUPLICATES_SUM, errval);
+    }
+    
+    free(final_ja);
+    free(final_ia);
+    free(final_a);
+}
+
 void backward_sblas(HMM *hmm, const unsigned int *Y, const unsigned int T, double * scalingFactor, double * beta){
     
     unsigned int i;
@@ -34,7 +82,7 @@ void backward_sblas(HMM *hmm, const unsigned int *Y, const unsigned int T, doubl
         printf("Error initializing the library!\n");
     }
     struct rsb_mtx_t ** mtx = malloc(hmm->observations*sizeof(struct rsb_mtx_t *));
-    emission_rsb_mtx(hmm, new_emission_probs, mtx, &errval);
+    b_emission_rsb_mtx(hmm, new_emission_probs, mtx, &errval);
 
     for(i = 0; i < hmm->observations; i++){
         free(new_emission_probs[i]);
