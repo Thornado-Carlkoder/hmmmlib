76dc5bdb3bf7cd6371f0ed7bc7674adf3c38cb7f
Carl M. Kobel <kobel@pm.me>
2019-11-21 15:03:10 +0100

    linux_build.sh formatting

--- hmmmlib/linux_build.sh
+++ hmmmlib/linux_build.sh
@@ -4,29 +4,30 @@
 
 # Run fresh cmake
 fresh_cmake () {
+    echo -e "Initializing fresh cmake build:"
     rm -r build
     mkdir build
     cd build
-    cmake .. && echo -e "cmake successfull\n"
+    cmake .. && echo -e "cmake: success\n"
 }
 
 
 # Run make
 run_make () {
-    make && echo -e "Built into build/\nmake successful"
+    make && echo -e "Built into build/\nmake: success\n"
 }
 
 
 # Test exe
 test_exe () {
-    echo -e "\nExecuting executable:"
-    ../build/exeHMMLIB
+    echo -e "Executing executable:"
+    ../build/exeHMMLIB && echo -e "executable: success\n"
 }
 
 # Test pinding
 test_pinding () {
-    echo -e "\nRunning python binding test(s):"
-    python ../../test_framework/test.py; 
+    echo -e "Running python binding test(s):"
+    python ../../test_framework/test.py && echo -e "python binding tests: success"; 
 }
 
 
