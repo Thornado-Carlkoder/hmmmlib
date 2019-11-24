2c55634bae5c30b8551ebe7c423b990e339d87c2
Carl M. Kobel <kobel@pm.me>
2019-11-24 16:48:00 +0100

    state vs sparse

--- test_framework/running_time.py
+++ test_framework/running_time.py
@@ -312,7 +312,7 @@ if __name__ == "__main__" :
         standard_test_sparseness("backward_time", "CSR", inputsize, hidden_states, start, stop, increment, file)
         standard_test_sparseness("baumWelch", "CSR", inputsize, hidden_states, start, stop, increment, file, str(1), n_iterations = 1)"""
 
-        for hidden_states in [16, 8, 4]:
+        for hidden_states in [32, 16, 8, 4]:
         
             print('# hs:', hidden_states, file = sys.stderr)
             ## Conventional #
@@ -327,6 +327,12 @@ if __name__ == "__main__" :
             ## CSR #
             standard_test_sparseness("forward", "CSR", inputsize, hidden_states, start, stop, increment, file, hidden_states)
             standard_test_sparseness("backward_time", "CSR", inputsize, hidden_states, start, stop, increment, file, hidden_states)
+
+            ## RSB #
+            standard_test_sparseness("forward", "RSB", inputsize, hidden_states, start, stop, increment, file, hidden_states)
+            standard_test_sparseness("backward_time", "RSB", inputsize, hidden_states, start, stop, increment, file, hidden_states)
+
+            
             
 
 
