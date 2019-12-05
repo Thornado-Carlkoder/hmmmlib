6ff136535dbe62247e79b29a6ca49b66a21a2e9b
Carl M. Kobel <kobel@pm.me>
2019-12-05 15:11:51 +0100

    fig B1

---
+++ test_framework/running_time_B1.py
@@ -0,0 +1,52 @@
+from running_time import *
+import binding as hmm_binding
+import random, decimal
+import time, sys, os
+
+
+
+print('## Testing varying sparseness of transition and emission matrices. ##', file = sys.stderr)
+replicates = 5
+input_size = 100000
+alphabet_size = 4
+
+start = 0
+stop = 1.001
+increment = 0.1
+#file = os.path.dirname(os.path.realpath(__file__)) + "/data/pantro3_X.fasta"
+
+if True:
+    print('test, observations, time, algorithm, variant, statespace')
+
+for hidden_states in [300, 100, 30, 10]:
+
+    for algorithm in ['forward', 'backward_time']:
+        for hmmType in ['BLAS', 'RSB']:
+    
+            #standard_test_sparseness(algorithm, hmmType, input_size, hidden_states, start, stop, increment, file, hidden_states)
+
+            # Generate data
+            test_standard_data = random.choices([choice for choice in range(alphabet_size)], k = input_size)  # Generates a data set with an arbitrary alphabet_size size (uniform).
+
+            for sparseness in float_range(start, stop, increment): # increasing sparseness
+                print(f'{algorithm}\t{round(sparseness,3)}', file = sys.stderr, end = '\t', flush = True)
+
+
+
+                for _ in range(replicates):
+                    print('r', end = '', file = sys.stderr, flush = True)
+                    
+                    o = hmm_binding.binded_HMM(hidden_states, alphabet_size, hmmType = hmmType)
+                    
+                    set_random(o, sparseness)    
+
+                    t0 = time.time()
+                    getattr(o, algorithm)(test_standard_data)
+                    t1 = time.time()
+
+                    o.deallocate()
+
+                    print(f'sparseness, {round(sparseness, 4)}, {t1-t0}, {algorithm}, {o.hmmType}, {hidden_states}')
+                print('', file = sys.stderr, flush = True) # newline
+                
+
