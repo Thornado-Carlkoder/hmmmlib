f4ab2028ac7daacf0f0f41f82a4dbdf13590678d
Carl M. Kobel <kobel@pm.me>
2019-12-09 11:41:09 +0100

    add B for merge into RSB

--- test_framework/run_save_all_test.sh
+++ test_framework/run_save_all_test.sh
@@ -3,7 +3,8 @@
 mkdir running_times
 python running_time_A1.py > running_times/running_time_A1.csv
 python running_time_A2.py > running_times/running_time_A2.csv
-
+#python running_time_B1.py > running_times/running_time_A2.csv
+#python running_time_B2.py > running_times/running_time_A2.csv
 python running_time_C1.py > running_times/running_time_C1.csv
 python running_time_C2.py > running_times/running_time_C2.csv
 python running_time_C3.py > running_times/running_time_C3.csv
