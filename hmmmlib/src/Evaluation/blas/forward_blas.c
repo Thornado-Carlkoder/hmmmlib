6f1795cf15235943dde4463ecfdf55827acdb22e
Carl Mathias Kobel <kobel@pm.me>
2020-01-03 16:45:41 +0100

    Update forward_blas.c

--- hmmmlib/src/Evaluation/blas/forward_blas.c
+++ hmmmlib/src/Evaluation/blas/forward_blas.c
@@ -15,14 +15,14 @@ void forward_blas(HMM *hmm, const unsigned int *Y, const unsigned int T, double 
         
         // A diagonal matrix 'matrix[j]' is created for each symbol in the alphabet
         // It contains the probability of emitting the i'th symbol from state j
-        for(j = 0; j < hmm->hiddenStates; j++){ // (For each hidden state in the state space.)
+        for(j = 0; j < hmm->hiddenStates; j++){
             matrix[j*hmm->hiddenStates+j] = hmm->emissionProbs[j*hmm->observations+i]; 
         }
         
         
         // If the i'th symbol is equal to the first in the data:
-        // Use BLAS to compute the following:
-        // alpha :=  matrix * hmm->initProbs + alpha
+        //
+        // BLAS alpha :=  matrix * hmm->initProbs + alpha
         if(i == Y[0]){ 
             cblas_dsymv(CblasRowMajor, 121, hmm->hiddenStates,   1.0, matrix, hmm->hiddenStates, hmm->initProbs,    1,    1, alpha,    1);
             //          Layout        uplo,                 n, alpha,     *a,               lda,              x, incx, beta,     y, incy
@@ -30,8 +30,8 @@ void forward_blas(HMM *hmm, const unsigned int *Y, const unsigned int T, double 
         
         double * emission_probs = calloc(hmm->hiddenStates*hmm->hiddenStates, sizeof(double));
         
-        // Use BLAS to calculate the following:
-        // emission_probs = hmm->transitionProbs * matrix      
+        
+        // BLAS emission_probs = hmm->transitionProbs * matrix      
         cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, hmm->hiddenStates, hmm->hiddenStates, hmm->hiddenStates,     1.0, hmm->transitionProbs, hmm->hiddenStates, matrix, hmm->hiddenStates,  0.0, emission_probs, hmm->hiddenStates);
         //                  Order,       TransA,       TransB,                 M,                 N,                 K,   alpha,                    A,               lda,      B                ldb, beta,              C,               ldc
         
@@ -40,12 +40,20 @@ void forward_blas(HMM *hmm, const unsigned int *Y, const unsigned int T, double 
     }
     free(matrix);
     
+    
     scalingFactor[0] = cblas_dasum(hmm->hiddenStates, alpha, 1);
+    
+    // BLAS alpha = alpha / scalingFactor[0]
     cblas_dscal(hmm->hiddenStates, (1.0/scalingFactor[0]), alpha, 1);
 
     for(i = 1; i<T; i++){
-        cblas_dgemv(CblasRowMajor, CblasTrans, hmm->hiddenStates, hmm->hiddenStates, 1.0, new_emission_probs[Y[i]], hmm->hiddenStates, alpha+hmm->hiddenStates*(i-1), 1, 0, alpha+hmm->hiddenStates*i, 1);
+        // Y = alpha AX + beta Y
+        // BLAS  ? = new_emission_probs[Y[i]] * alpha+hmm->hiddenStates*(i-1) 
+        cblas_dgemv(CblasRowMajor, CblasTrans, hmm->hiddenStates, hmm->hiddenStates,   1.0, new_emission_probs[Y[i]], hmm->hiddenStates, alpha+hmm->hiddenStates*(i-1),    1,    0, alpha+hmm->hiddenStates*i,    1);
+        //                  order,     TransA,                 M,                 N, alpha,                        A,               lda,                             X, incX, beta,                         Y, incY                                                                                                                                         
+        
         scalingFactor[i] = 1.0/cblas_dasum(hmm->hiddenStates, alpha+hmm->hiddenStates*i, 1);
+        // BLAS alpha+hmm->hiddenStates*i / scalingFactor[i]
         cblas_dscal(hmm->hiddenStates, scalingFactor[i], alpha+hmm->hiddenStates*i, 1);
         
     }
